0dc3ea22e18f3a02eff17b73671704316acd9b82
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-04-29 18:17:06 -0300

    bug fixes and communication setting up

--- address_book_impl.py
+++ address_book_impl.py
@@ -1,9 +1,40 @@
 import CorbaAddressBook, CorbaAddressBook__POA
+from omniORB import CORBA
+import CosNaming
 
 class AddressBookImpl (CorbaAddressBook__POA.AddressBook):
 
 	def __init__(self):
 		self.contact_list = {}
+		self.all_books = []
+
+	def loadBooks(self, naming, index):
+		self.naming = naming
+		self.index = index
+
+		for i in range(1, 5):
+			if (i == self.index):
+				self.all_books.append(None)
+				continue
+			
+			self.all_books.append(self.getAddressBook(i))
+		
+	def getAddressBook(self, i):
+		# Resolve the name "test.my_context/AddressBook.#"
+		name = [CosNaming.NameComponent("test", "my_context"),
+						CosNaming.NameComponent("AddressBook", str(i))]
+		try:
+			objRef = self.naming.resolve(name)
+			objRef._non_existent() # Test if obj exists
+
+		except (CosNaming.NamingContext.NotFound, CORBA.TRANSIENT):
+			pass
+
+		else:
+			# Narrow the object to an CorbaAddressBook::AddressBook
+			return objRef._narrow(CorbaAddressBook.AddressBook)
+		
+		return None
 
 	def addContact(self, c):
 		if (c.name not in self.contact_list):
@@ -20,7 +51,11 @@ class AddressBookImpl (CorbaAddressBook__POA.AddressBook):
 	def updateContact(self, currentName, updatedContact):
 		if (currentName in self.contact_list):
 			if (updatedContact.name != currentName):
-				self.delContact(currentName)
+				try:
+					self.delContact(currentName)
+
+				except (CorbaAddressBook.ContactNotFound):
+					pass
 			
 			self.contact_list[updatedContact.name] = updatedContact
 		else:
@@ -33,6 +68,7 @@ class AddressBookImpl (CorbaAddressBook__POA.AddressBook):
 		except (CorbaAddressBook.ContactAlreadyExists):
 			try:
 				self.updateContact(c.name, c)
+			
 			except (CorbaAddressBook.ContactNotFound):
 				pass
 
